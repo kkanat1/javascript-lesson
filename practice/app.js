ab1444a4531dae62efb4c02dcf0b7840663ab53d
kanata <kanata13131@gmail.com>
2024-12-16 14:35:59 +0900

    feat:first commit

---
+++ practice/app.js
@@ -0,0 +1,60 @@
+// console.log('hello','alfa');
+
+// let i = 'js';
+// let greet = 'hello';
+// const num = 6;
+// console.log(i,num);
+// console.log(`${greet}
+// kanata`);
+// let num2 = 8;
+// console.log(num + num2);
+// console.log(num * num2);
+// let nameA = ['john','adf', 'asdf'];
+// console.log(nameA);
+// console.log(nameA[2]);
+// console.log('lenght is ' + nameA.length);
+
+// let jogn = {
+//   height:184,
+//   weight:12,
+//   bloodType:'B',
+// }
+// console.log('Bloodtype of jogn is(object) ' + jogn.bloodType);
+
+// function doFunc(callback) {
+//   console.log('doFuncが呼び出されました');
+//   callback();
+// }
+
+// function output() {
+//   console.log('コールバック関数が呼び出されました');
+// }
+// doFunc(output);
+
+let random = Math.random(); // 0~1までの乱数の生成
+console.log('random => ', random);
+
+for (let i = 0;i < 10; i++) {
+  console.log('forLoop ' + i);
+
+}
+
+
+// let one = '1';
+// let two = 2;
+
+// console.log('one →', typeof one, one); // one → string 1
+// console.log('two →', typeof two, two); // one → number 2
+
+// let floor = Math.floor(4.98); // 切り下げ
+// console.log('floor => ', floor);
+
+
+// let myAge = 60;
+// if (myAge >= 35) {
+//   console.log('健康診断でバリウムを飲みます');//display only this 
+// } else if (myAge >= 20) {
+//   console.log('お酒が飲めます');
+// } else if (myAge >= 18) {
+//   console.log('選挙権があります');
+// }
